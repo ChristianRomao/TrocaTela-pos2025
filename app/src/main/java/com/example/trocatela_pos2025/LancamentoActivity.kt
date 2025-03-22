d31e2ada21ba9cc71d65323f4bbcce2680b1b25a
Christian Romão <chritiangabriel4@gmail.com>
2025-03-22 08:08:55 -0300

    Adicao de novas situacoes

--- app/src/main/java/com/example/trocatela_pos2025/LancamentoActivity.kt
+++ app/src/main/java/com/example/trocatela_pos2025/LancamentoActivity.kt
@@ -1,21 +1,14 @@
 package com.example.trocatela_pos2025
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LancamentoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-    fun btLancamentoOnClick(view: View) {
-        val intent = Intent(this, LancamentoActivity::class.java)
-        startActivity(intent)
+        setContentView(R.layout.activity_lancamento)
     }
 }
